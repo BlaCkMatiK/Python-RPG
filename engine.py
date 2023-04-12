d4fc778382cd92e9ad2800e96be317fba3da3fac
Mateo LALANNE DE MIRAS <lalannemateo@hotmail.com>
2023-04-13 00:13:24 +0200

    (ctrl+c) géré

--- engine.py
+++ engine.py
@@ -1,12 +1,22 @@
 #import rich
 import os
 import random
+import time
 
 from caracter import *
 from dice import Dice
 from evenement import *
 from screen import *
+import signal
+import threading
 
+def handle_signal(signal_num, frame):
+    os.system("cls")
+    print("Fermeture du jeu prématurée par l'utilisateur (Ctrl+C) \n3 secs. avant fermeture")
+    time.sleep(3)
+    exit(0)
+
+signal.signal(signal.SIGINT, handle_signal)
 
 def main():
     title()
@@ -18,5 +28,6 @@ def main():
     over()
 
 if __name__ == "__main__":
-    main()
+    while True:
+        main()
     
