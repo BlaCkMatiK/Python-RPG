88b307755fc362124f2c8f5dfaa648b720fe85cf
Mateo LALANNE DE MIRAS <lalannemateo@hotmail.com>
2023-04-15 14:11:10 +0200

    utilisation moteur sons + statistiques de fin

--- engine.py
+++ engine.py
@@ -9,25 +9,33 @@ from evenement import *
 from screen import *
 import signal
 import threading
+from sounds import *
 
 def handle_signal(signal_num, frame):
     os.system("cls")
-    print("Fermeture du jeu prématurée par l'utilisateur (Ctrl+C) \n3 secs. avant fermeture")
-    time.sleep(3)
+    print("Fermeture du jeu prématurée par l'utilisateur (Ctrl+C) \n1 sec. avant fermeture")
+    time.sleep(1)
     exit(0)
 
 signal.signal(signal.SIGINT, handle_signal)
 
 def start():
-    title()
-    os.system("cls")
+    title()    
+    sound_create()
+    menu()
 
-def main():    
+def main():
     player = create_character()
+    quit_pygame()
+    sound_enter()
+    time.sleep(10)
+    sound_bgm()
     #stats(player)
     while Character.is_alive(player):
         choose_event(player)
-    over()
+    #over()
+    print_game_over()
+    end_stats(player)
     main()
 
 if __name__ == "__main__":
