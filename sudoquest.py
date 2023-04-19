e36c9b1d58bc44989af4c94a448b87a552513714
Mateo LALANNE DE MIRAS <lalannemateo@hotmail.com>
2023-04-19 03:18:21 +0200

    inventaire + skip histoire

--- sudoquest.py
+++ sudoquest.py
@@ -3,13 +3,16 @@ import os
 import random
 import time
 
-from caracter import *
+from caracter import create_character
 from dice import Dice
 from evenement import *
-from screen import startup, story
+from screen import *
 import signal
 import threading
 from sounds import *
+
+from commandes import Commandes
+from inventaire import Inventaire
 
 def handle_signal(signal_num, frame):
     os.system("cls")
@@ -23,21 +26,26 @@ signal.signal(signal.SIGINT, handle_signal)
 def start():
     startup()    
     sound_create()
-    story()
+    if input("Passer histoire ? (Oui / Non) - ").lower().startswith("o"):
+        pass
+    else :
+        story()
 
 def main():
-    player = create_character()
+    player_b = create_character()
+    stuff = Inventaire(player_b)
     quit_pygame()
     sound_enter()
     time.sleep(10)
     os.system("cls")
     sound_bgm()
     #stats(player)
-    while Character.is_alive(player):
-        choose_event(player)
+    while player_b.is_alive():
+        action = Commandes(player_b, stuff)
+        action.choix(player_b, stuff)
     #over()
     print_game_over()
-    end_stats(player)
+    end_stats(player_b)
     main()
 
 if __name__ == "__main__":
